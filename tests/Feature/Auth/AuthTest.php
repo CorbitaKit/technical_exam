43881d5d5b410fac81569d60f476b00aa3e8d6c9
Kit Corbita <corbita.khatezz@gmail.com>
2023-09-19 14:42:26 +0800

    First commit

---
+++ tests/Feature/Auth/AuthTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_user_can_log_in()
+    {
+        
+    }
+}
